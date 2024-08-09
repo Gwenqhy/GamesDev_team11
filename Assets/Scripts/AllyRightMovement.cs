270ca639834b91fa0d8f44f6f21d20cc1587b3c4
Cydjdjd <foochuanyong@gmail.com>
2024-08-09 20:00:51 +0800

    updated movement,add health to tower and ally and collision damage

---
+++ Assets/Scripts/AllyRightMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllyRightMovement : MonoBehaviour
+{
+    public Rigidbody2D robotRb;
+    public int speed;
+    private void FixedUpdate()
+    {
+        robotRb.velocity = Vector2.right * speed;
+    }
+}
---
+++ Assets/Scripts/AllyUpMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllyUpMovement : MonoBehaviour
+{
+    public Rigidbody2D robotRb;
+    public int speed;
+    private void FixedUpdate()
+    {
+        robotRb.velocity = Vector2.up * speed;
+    }
+}
