677cf11b5ea447433a928a2c0c1990b2ece1b25e
Irvin <irvintoh20@gmail.com>
2024-08-12 16:34:30 +0800

    added some scripting for turret

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    private Camera _camera;
+    [SerializeField, Range(1, 100)] private float rotationSpd = 1;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mousePos = _camera.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+
+        transform.up = Vector3.MoveTowards(transform.up, mousePos, rotationSpd * Time.deltaTime);
+    }
+}
